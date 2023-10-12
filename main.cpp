33ae54f1e3a718d703cc4b9c86fe57e04a21a803
Jacek <jacek.kurzydlo@protonmail.ch>
2023-10-12 12:22:55 +0200

    Commit do usunięcia 2

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ int main(){
     lista.wstawNaPoczatek(6);gn
     lista.wstawNaPoczatek(7);
     lista.wstawNaKoniec(10);
-    lista.usunZPozycji(2);
+    lista.usunZPozycji(2);xdfcgvhbj
     lista.wyswietl();
     //lista.wyswietlOdwrocona();
     //commit usun z poczatku i konca i pozycji 
